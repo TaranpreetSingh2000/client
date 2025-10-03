c0c858dd0dd030bd9c95245e06439bd0100c4aea
TaranpreetSingh2000 <88654743+TaranpreetSingh2000@users.noreply.github.com>
2025-10-03 10:52:07 +0530

    configure the nextConfig.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, // disables sharp
+  },
+  experimental: {
+    optimizeCss: false, // disables lightningcss
+  },
+};
 
 export default nextConfig;
